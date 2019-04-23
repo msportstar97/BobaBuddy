742f2bae772a642c8e131830466dd0d39d9ad5b5
Michelle Giang <michi.giang@gmail.com>
2019-04-23 17:17:34 -0500

    search results appear

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -2,6 +2,7 @@ import './Results.scss';
 import React, { Component } from 'react';
 import Header from '../Header/Header.js';
 import axios from 'axios';
+// import GoogleMapReact from 'google-map-react';
 
 class Results extends Component {
   constructor() {
@@ -9,7 +10,7 @@ class Results extends Component {
     
     this.state = {
       geo: {},
-      results: []
+      results: [],
     }
 
     this.apiKey = '&key=AIzaSyDFtzabY5k6-NOC6V1h3b-LjftEvZyW2MY';
@@ -24,32 +25,29 @@ class Results extends Component {
     const {place} = this.props.location.state;
 
     let locUrl = `${this.geolocUrl}${place.replace(/\s/g,'+')}${this.apiKey}`;
-
+    console.log(locUrl);
     axios.get(locUrl).then((response) => {
       this.setState({
         geo: response.data.results[0].geometry.location
       })
 
-      let resUrl = `${this.searchUrl}${this.state.geo.lat},${this.state.geo.lng}&rankby=distance&keyword=boba${this.apiKey}`;
-      console.log(resUrl);
-      axios.get(resUrl, {
-        config: {
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Content-Type': 'application/json',
-          }
-        }
-      }).then((response) => {
-        this.setState({
-          results: response.data.results
-        })
-        console.log(this.state.results);
-      }).catch((error) => {
-        console.log(error);
-      });
+      // let resUrl = `${this.searchUrl}${this.state.geo.lat},${this.state.geo.lng}&rankby=distance&keyword=boba${this.apiKey}`;
+      // console.log(resUrl);
 
+      const placesRequest = {
+        location: new window.google.maps.LatLng(this.state.geo.lat, this.state.geo.lng),
+        query: '(bubble tea) OR (boba)',
+        radius: 50,
+      };
+
+      let map = new window.google.maps.Map(document.createElement('div'));
+      let service = new window.google.maps.places.PlacesService(map);
+
+      service.textSearch(placesRequest, ((response) => {
+        this.setState({
+          results: response
+        })
+      }))
     }).catch((error) => {
       console.log(error);
     });
@@ -62,9 +60,11 @@ class Results extends Component {
         <Header />
         <div className="cards">
           {this.state.results.map((boba, idx) =>
-            <div className="bobaPlace">
+            <div className="bobaPlace" key={idx}>
               <p>{boba.name}</p>
               <p>Location: {boba.formatted_address}</p>
+              <br></br>
+              <br></br>
             </div>
           )}
         </div>
