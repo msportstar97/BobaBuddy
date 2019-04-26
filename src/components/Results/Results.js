b68b5fb60c8e780a11aae2f4b320a10878c05cfb
charisdelcielo <chaewonryu@gmail.com>
2019-04-26 03:22:37 -0500

    filter

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,6 +1,8 @@
 import './Results.scss';
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Dropdown } from 'semantic-ui-react'
+import { Input } from 'semantic-ui-react'
 // import GoogleMapReact from 'google-map-react';
 
 class Results extends Component {
@@ -10,6 +12,9 @@ class Results extends Component {
     this.state = {
       geo: {},
       results: [],
+      filterOption: "distance",
+      showDrinkDropdown: false,
+      drinkFilterOption: ""
     }
 
     this.apiKey = '&key=AIzaSyDFtzabY5k6-NOC6V1h3b-LjftEvZyW2MY';
@@ -43,6 +48,7 @@ class Results extends Component {
         this.setState({
           results: response
         })
+        console.log(this.state.results);
       }))
     }).catch((error) => {
       console.log(error);
@@ -50,14 +56,85 @@ class Results extends Component {
 
   }
 
+  handleFilter = (e) => {
+
+    if (e.target.value == "drink-rating") {
+      this.setState({showDrinkDropdown: true});
+    }
+
+  }
+
+
+
    render() {
+
+     const filterOptions = [
+       {
+         key: "peach oolong tea",
+         value: "peach oolong tea",
+         text: "Peach Oolong Tea"
+       },
+       {
+         key: "oolong tea",
+         value: "oolong tea",
+         text: "Oolong Tea"
+       },
+       {
+         key: "oolong milk tea",
+         value: "oolong milk tea",
+         text: "Oolong Milk Tea"
+       }
+     ]
+
+     function handleDrinkFilter(e, {value}) {
+       //this.setState({drinkFilterOption: {value}})
+       console.log({value})
+     }
+
+
+
+      var createReactClass = require('create-react-class');
+
+
+      var DrinkDropdown = createReactClass({
+        render: function() {
+        return (
+          <div id="results" className="search-results">
+            <Dropdown
+              placeholder='Select Drink'
+              fluid
+              search
+              selection
+              options={filterOptions}
+              onChange={(e, { value }) => handleDrinkFilter(e, { value })}
+            />
+          </div>
+        );
+      }
+    });
+
+
+
+
      return (
        <div className="results">
+        <div className = "filterDropdown">
+          <span> Sort By </span>
+            <select className = "order-select" onChange={(e) => this.handleFilter(e)}>
+              <option value="distance">Distance</option>
+              <option value="rating">Rating</option>
+              <option value="drink-rating">Drink Rating</option>
+            </select>
+            { this.state.showDrinkDropdown ? <DrinkDropdown /> : null }
+        </div>
+
         <div className="cards">
           {this.state.results.map((boba, idx) =>
             <div className="bobaPlace" key={idx}>
               <p>{boba.name}</p>
-              <p>Location: {boba.formatted_address}</p>
+              <p id = "place_rating"> {boba.rating} / 5.0 </p>
+              <p>{boba.formatted_address}</p>
+
             </div>
           )}
 
